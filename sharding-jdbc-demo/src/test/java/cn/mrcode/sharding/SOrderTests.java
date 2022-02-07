89add3e6084e7b14c9a8f5fe6c5f494a766ccc3a
zq99299 <99299684@qq.com>
2022-02-07 21:10:46 +0800

    sharding-jdbc 分布式事务 - 本地事务测试

--- sharding-jdbc-demo/src/test/java/cn/mrcode/sharding/SOrderTests.java
+++ sharding-jdbc-demo/src/test/java/cn/mrcode/sharding/SOrderTests.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
@@ -31,5 +32,30 @@ public class SOrderTests {
         order.setOrderAmount(BigDecimal.TEN);
         order.setOrderStatus(1);
         sOrderMapper.insertSelective(order);
+    }
+
+    /**
+     * 分布式事务测试：本地事务
+     */
+    @Test
+    @Transactional
+    public void testOrder2() {
+        SOrder order = new SOrder();
+        // 数据库：userId 偶数分到 sharding-order，奇数分到 shard-order
+        order.setUserId(19);
+        // 表：id ，使用雪花算法生成，偶数分到 s_order_1,基数分到 s_order_2
+        // order.setId(1);
+        order.setOrderAmount(BigDecimal.TEN);
+        order.setOrderStatus(-1);
+        sOrderMapper.insertSelective(order);
+
+        SOrder order2 = new SOrder();
+        // 数据库：userId 偶数分到 sharding-order，奇数分到 shard-order
+        order2.setUserId(20);
+        // 表：id ，使用雪花算法生成，偶数分到 s_order_1,基数分到 s_order_2
+        // order.setId(1);
+        order2.setOrderAmount(BigDecimal.TEN);
+        order2.setOrderStatus(-1);
+        sOrderMapper.insertSelective(order2);
     }
 }
